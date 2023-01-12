7708e48a7d50960bd3de9998f1262667539627c0
Steven Young <sy3152@nyu.edu>
2023-01-12 08:29:09 -0800

    230111

---
+++ MR_data_collection/random_action_230111.py
@@ -0,0 +1,199 @@
+##!/usr/bin/env python
+import random
+import rospy
+from geometry_msgs.msg import Twist
+import time
+PI = 3.1415926535897
+
+
+def random_move():
+    # Use the random.randint() function to choose a number between 0 and 3
+    move = random.randint(0, 3)
+    if move == 0:
+        move_forward()
+    elif move == 1:
+        move_backward()
+    elif move == 2:
+        move_left()
+    elif move == 3:
+        move_right()
+
+def move_forward():
+    # Starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+    
+    #Receiveing the user's input
+    print("move robot forward")
+    # speed = input("Input your speed:")
+    # distance = input("Type your distance:") 
+    speed = 1
+    distance = 1
+
+    vel_msg.linear.x = abs(speed)
+    #Since we are moving just in x-axis
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
+    
+    while not rospy.is_shutdown(): # is a common construct used in ROS Python scripts to run a loop until the ROS node is shut down.
+
+        #Setting the current time for distance calculus
+        t0 = float(rospy.Time.now().to_sec())
+        current_distance = 0
+
+        #Loop to move the turtle in an specified distance
+        while(current_distance < distance):
+            #Publish the velocity
+            velocity_publisher.publish(vel_msg)
+            #Takes actual time to velocity calculus
+            t1=float(rospy.Time.now().to_sec())
+            #Calculates distancePoseStamped
+            current_distance= speed*(t1-t0)
+        #After the loop, stops the robot
+        vel_msg.linear.x = 0
+        #Force the robot to stop
+        velocity_publisher.publish(vel_msg)
+        break
+
+def move_backward():
+    # Starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+    
+    #Receiveing the user's input
+    print("move robot backward")
+    # speed = input("Input your speed:")
+    # distance = input("Type your distance:")
+    speed = 1
+    distance = 1
+
+    vel_msg.linear.x = -abs(speed)
+    #Since we are moving just in x-axis
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
+    
+    while not rospy.is_shutdown():
+
+        #Setting the current time for distance calculus
+        t0 = float(rospy.Time.now().to_sec())
+        current_distance = 0
+
+        #Loop to move the turtle in an specified distance
+        while(current_distance < distance):
+            #Publish the velocity
+            velocity_publisher.publish(vel_msg)
+            #Takes actual time to velocity calculus
+            t1=float(rospy.Time.now().to_sec())
+            #Calculates distancePoseStamped
+            current_distance= speed*(t1-t0)
+        #After the loop, stops the robot
+        vel_msg.linear.x = 0
+        #Force the robot to stop
+        velocity_publisher.publish(vel_msg)
+        break
+
+
+def move_left():
+    #Starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+
+    # Receiveing the user's input
+    print("move robot left")
+    speed = 45 #degrees/sec
+    angle =  90 #degrees
+    clockwise = 0 #True or false
+
+    #Converting from angles to radians
+    angular_speed = speed*2*PI/360 #
+    relative_angle = angle*2*PI/360  #
+
+    #We wont use linear components
+    vel_msg.linear.x=0
+    vel_msg.linear.y=0
+    vel_msg.linear.z=0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+
+    # Checking if our movement is CW or CCW
+    if clockwise:
+        vel_msg.angular.z = -abs(angular_speed)
+    else:
+        vel_msg.angular.z = abs(angular_speed)
+    # Setting the current time for distance calculus
+    t0 = rospy.Time.now().to_sec()
+    current_angle = 0
+
+    while(current_angle < relative_angle):
+        velocity_publisher.publish(vel_msg)
+        t1 = rospy.Time.now().to_sec()
+        current_angle = angular_speed*(t1-t0)
+
+
+    #Forcing our robot to stop
+    vel_msg.angular.z = 0
+    velocity_publisher.publish(vel_msg)
+    # rospy.spin()
+
+def move_right():
+    #Starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+
+    # Receiveing the user's input
+    print("move robot right")
+    speed = 45 #degrees/sec
+    angle =  90 #degrees
+    clockwise = 1 #True or false
+
+    #Converting from angles to radians
+    angular_speed = speed*2*PI/360 #
+    relative_angle = angle*2*PI/360  #
+
+    #We wont use linear components
+    vel_msg.linear.x=0
+    vel_msg.linear.y=0
+    vel_msg.linear.z=0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+
+    # Checking if our movement is CW or CCW
+    if clockwise:
+        vel_msg.angular.z = -abs(angular_speed)
+    else:
+        vel_msg.angular.z = abs(angular_speed)
+    # Setting the current time for distance calculus
+    t0 = rospy.Time.now().to_sec()
+    current_angle = 0
+
+    while(current_angle < relative_angle):
+        velocity_publisher.publish(vel_msg)
+        t1 = rospy.Time.now().to_sec()
+        current_angle = angular_speed*(t1-t0)
+
+
+    #Forcing our robot to stop
+    vel_msg.angular.z = 0
+    velocity_publisher.publish(vel_msg)
+    # rospy.spin()
+
+
+
+if __name__ == '__main__':
+    try:
+        for count in range(10):
+            random_move()
+            time.sleep(1)
+    except rospy.ROSInterruptException: pass
+
+
---
+++ MR_data_collection/turtlebot_move_forward_backward_test.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# move forward and backward specif distance (but I think it remains some problem)
+import rospy
+from geometry_msgs.msg import Twist
+import time
+
+def move_forward():
+    # Starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+    
+    #Receiveing the user's input
+    print("move robot forward")
+    # speed = input("Input your speed:")
+    # distance = input("Type your distance:") 
+    speed = 0.05
+    distance = 0.1
+
+    vel_msg.linear.x = abs(speed)
+    #Since we are moving just in x-axis
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
+    
+    while not rospy.is_shutdown(): # is a common construct used in ROS Python scripts to run a loop until the ROS node is shut down.
+
+        #Setting the current time for distance calculus
+        t0 = float(rospy.Time.now().to_sec())
+        current_distance = 0
+
+        #Loop to move the turtle in an specified distance
+        while(current_distance < distance):
+            #Publish the velocity
+            velocity_publisher.publish(vel_msg)
+            #Takes actual time to velocity calculus
+            t1=float(rospy.Time.now().to_sec())
+            #Calculates distancePoseStamped
+            current_distance= speed*(t1-t0)
+        #After the loop, stops the robot
+        vel_msg.linear.x = 0
+        #Force the robot to stop
+        velocity_publisher.publish(vel_msg)
+        break
+
+def move_backward():
+    # Starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+    
+    #Receiveing the user's input
+    print("move robot backward")
+    # speed = input("Input your speed:")
+    # distance = input("Type your distance:")
+    speed = 0.05
+    distance = 0.1
+
+    vel_msg.linear.x = -abs(speed)
+    #Since we are moving just in x-axis
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
+    
+    while not rospy.is_shutdown():
+
+        #Setting the current time for distance calculus
+        t0 = float(rospy.Time.now().to_sec())
+        current_distance = 0
+
+        #Loop to move the turtle in an specified distance
+        while(current_distance < distance):
+            #Publish the velocity
+            velocity_publisher.publish(vel_msg)
+            #Takes actual time to velocity calculus
+            t1=float(rospy.Time.now().to_sec())
+            #Calculates distancePoseStamped
+            current_distance= speed*(t1-t0)
+        #After the loop, stops the robot
+        vel_msg.linear.x = 0
+        #Force the robot to stop
+        velocity_publisher.publish(vel_msg)
+        break
+
+
+if __name__ == '__main__':
+    try:
+        #Testing our function
+        move_forward()
+        time.sleep(1)
+        move_backward()
+    except rospy.ROSInterruptException: pass
+
